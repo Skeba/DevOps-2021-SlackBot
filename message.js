1eda1bad96466a4fb76ccb196a021e48ca8482a2
Skeba <seba.lundgren@gmail.com>
2021-10-26 18:21:06 +0300

    Slackbot
    Creates slackbot folder. Start of our slackbot

---
+++ message.js
@@ -0,0 +1,27 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set region
+AWS.config.update({region: 'us-east-1'});
+
+var myArgs = process.argv[1];
+console.log('myArgs: ', myArgs);
+
+// Create publish parameters
+var params = {
+  Message: myArgs[1], /* required */
+  TopicArn: 'arn:aws:sns:us-east-1:572443860726:MyBotTopic'
+};
+
+
+// Create promise and SNS service object
+var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+
+// Handle promise's fulfilled/rejected states
+publishTextPromise.then(
+  function(data) {
+    console.log(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
+    console.log("MessageID is " + data.MessageId);
+  }).catch(
+    function(err) {
+    console.error(err, err.stack);
+  });
